f3736dd356cfecdaa140adbe806c0a138a07dc9f
Mike Ritchie <mike.ritchie@ticketops.com>
2020-04-08 20:18:36 -0700

    switched to using constants inside Bin2Mod3

--- dfas/bin2mod3.js
+++ dfas/bin2mod3.js
@@ -1,21 +1,27 @@
 const BaseDFA = require('./basedfa');
 
 class Bin2Mod3 extends BaseDFA {
-    possibleStates = ['S0', 'S1', 'S3'];
-    possibleInputs = ['0', '1'];
-    initialState = 'S0';
-    allowedStates = ['S0', 'S1', 'S2'];
+    static STATE0 = 'S0';
+    static STATE1 = 'S1';
+    static STATE2 = 'S2';
+    static INPUT0 = '0';
+    static INPUT1 = '1';
+
+    possibleStates = [ Bin2Mod3.STATE0, Bin2Mod3.STATE1, Bin2Mod3.STATE2 ];
+    possibleInputs = [ Bin2Mod3.INPUT0, Bin2Mod3.INPUT1 ];
+    initialState = Bin2Mod3.STATE0;
+    allowedStates = [ Bin2Mod3.STATE0, Bin2Mod3.STATE1, Bin2Mod3.STATE2 ];
 
     reducer(intlState, input) {
         switch( intlState ) {
-            case 'S0' :
-                return ( input === '0' ) ? 'S0' : 'S1';
+            case Bin2Mod3.STATE0 :
+                return ( input === Bin2Mod3.INPUT0 ) ? Bin2Mod3.STATE0 : Bin2Mod3.STATE1;
 
-            case 'S1' :
-                return ( input === '0' ) ? 'S2' : 'S0';
+            case Bin2Mod3.STATE1 :
+                return ( input === Bin2Mod3.INPUT0 ) ? Bin2Mod3.STATE2 : Bin2Mod3.STATE0;
 
-            case 'S2' :
-                return ( input === '0' ) ? 'S1' : 'S2';
+            case Bin2Mod3.STATE2 :
+                return ( input === Bin2Mod3.INPUT0 ) ? Bin2Mod3.STATE1 : Bin2Mod3.STATE2;
 
             default :
                 return null;
